3930d6b200691c5a5457cae8e6a2859f93ec42c5
Velezosoriofabio <velezosoriofabio@gmail.com>
2024-10-09 13:05:31 -0500

    diseño navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
 <div class="container">
-  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-  <a class="navbar-brand" href="#">CRUD Pacientes</a>
+  <nav class="navbar navbar-expand-lg navbar-light">
+  <!-- <a class="navbar-brand" href="#">GRUPO SPAM</a> -->
+
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
+    <ul class="nav nav-pills">
       <li class="nav-item">
-        <router-link to="/Listar" class="nav-link">Listar pacientes</router-link>
+        <router-link to="/" class="nav-link">GRUPO SPAM</router-link>
       </li>
       <li class="nav-item">
-        <router-link to="/Crear" class="nav-link">Crear nuevo paciente</router-link>
+        <router-link to="/Listar" class="nav-link" >Listar pacientes</router-link>
       </li>
       <li class="nav-item">
-        <router-link to="/consultar" class="nav-link">Consultar Resultados</router-link>
+        <router-link to="/Crear" class="nav-link" >Crear nuevo paciente</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/consultar" class="nav-link" >Consultar Resultados</router-link>
       </li>
     </ul>
   </div>
@@ -32,15 +36,31 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 90px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px 20px;
+  color: #000; /* Color de texto por defecto */
+  background-color: transparent; /* Fondo transparente por defecto */
+  transition: background-color 0.3s, color 0.3s; /* Suave transición al activar */
+
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+  background-color: #42b983;
+}
+
+.nav-link.router-link-exact-active {
+    background-color: #2c3e50; /* Color de fondo para el enlace activo */
+    color: white; /* Color del texto cuando está activo */
+    border-radius: .25rem; /* Bordes redondeados como en Bootstrap */
+    transition: background-color 0.3s, color 0.3s; /* Transición suave */
+}
+
+.nav-link {
+    transition: background-color 0.3s, color 0.3s; /* Transición para cuando cambia a activo */
 }
 </style>
